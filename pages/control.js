7c340dcae3c79dd4fa42db497a56404e85149405
Matt Brennan <matt@153.io>
2017-02-03 10:52:58 +0000

    moar controls

--- pages/control.js
+++ pages/control.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {observe} from '../src/store';
+import pluralize from 'pluralize';
 
 const secondsInMinute = 60;
 const minutesInHour = 60;
@@ -23,11 +24,50 @@ const secondsIn = {
 	year: secondsInYear,
 };
 
-export default observe((props, {dispatch, subscribe}) => <main>{Object.keys(secondsIn).map(
-	period => <button key={period} onClick={() => dispatch('date', date => date + secondsIn[period])}>
-		+1 {period}
-	</button>
-)}
-<input defaultValue={subscribe('date')} onChange={ev => dispatch('_date', () => ev.target.value)} />
-<button onClick={() => dispatch('date', () => subscribe('_date'))}>Set</button>
+const Inc = observe(({period, multiplier = 1}, {dispatch}) => <button
+	onClick={() => dispatch('date', date => date + secondsIn[period] * multiplier)}>
+	+{multiplier} {pluralize(period, multiplier)}
+</button>);
+
+export default observe((props, {dispatch, subscribe}) => <main>
+	<div>
+		<Inc period='minute' />
+		<Inc period='minute' multiplier={5} />
+		<Inc period='minute' multiplier={30} />
+	</div>
+
+	<div>
+		<Inc period='hour' />
+		<Inc period='hour' multiplier={2} />
+		<Inc period='hour' multiplier={6} />
+		<Inc period='hour' multiplier={12} />
+	</div>
+
+	<div>
+		<Inc period='day' />
+		<Inc period='day' multiplier={2} />
+	</div>
+
+	<div>
+		<Inc period='week' />
+		<Inc period='week' multiplier={2} />
+	</div>
+
+	<div>
+		<Inc period='month' />
+		<Inc period='month' multiplier={2} />
+		<Inc period='month' multiplier={6} />
+	</div>
+
+	<div>
+		<Inc period='year' />
+		<Inc period='year' multiplier={2} />
+		<Inc period='year' multiplier={5} />
+		<Inc period='year' multiplier={10} />
+	</div>
+
+	<div>
+		<input defaultValue={subscribe('date')} onChange={ev => dispatch('_date', () => ev.target.value)} />
+		<button onClick={() => dispatch('date', () => subscribe('_date'))}>Set</button>
+	</div>
 </main>);
