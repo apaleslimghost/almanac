077d6b7a3289224822a1a4a726af3d5bdc63b2c2
Matt Brennan <matt@153.io>
2017-02-07 12:31:24 +0000

    flex

--- src/group.js
+++ src/group.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import map from 'lodash.map';
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 import {observe} from './store';
 import * as components from '../src/components';
 
@@ -9,17 +9,20 @@ display: flex;
 flex-direction: ${({direction}) => direction};
 `;
 
-const GridChild = styled.div``;
+const GridChild = styled.div`
+${({flex}) => flex && css`flex: ${flex};`}
+`;
 
 const Grid = ({layout, direction = 'row', keys = []}) => <Split direction={direction}>
 	{layout.map((child, i) => React.isValidElement(child) ?
-		<GridChild key={keys.concat(i).join('.')} >{child}</GridChild> :
+		<GridChild key={keys.concat(i).join('.')} {...child.props}>{child}</GridChild> :
 		<Grid key={keys.concat(i).join('.')} keys={keys.concat(i)} layout={child} direction={direction === 'row' ? 'column': 'row'} />
 	)}
 </Split>;
 
 const mapLayout = (layout, which) => layout.map(child =>
 	Array.isArray(child) ? mapLayout(child, which)
+	: child.component    ? React.createElement(components[child.component][which], child)
 	: React.createElement(components[child][which])
 );
 
