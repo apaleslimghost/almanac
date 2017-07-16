d84ae940685479d7d2e80132e63157776b7a0793
Matt Brennan <matt@153.io>
2017-07-16 11:45:52 +0100

    more button things

--- client/card.js
+++ client/card.js
@@ -8,7 +8,17 @@ import {Cards, Types} from '../src/collections';
 import preventingDefault from '../src/preventing-default';
 
 import Toggler from './toggler';
-import {Card as CardPrimitive, Label, List, Input, Textarea, FormGroup} from './primitives';
+import {
+	Card as CardPrimitive,
+	Label,
+	List,
+	Input,
+	Textarea,
+	FormGroup,
+	Button,
+	Icon,
+	LabelBody,
+} from './primitives';
 import {Field, Form, Select} from './form';
 import CardSelect from './card-select';
 import {TypeSelect} from './link-type';
@@ -26,20 +36,46 @@ export const EditCard = ({card, saveCard, toggle, deleteCard}) =>
 		initialData={card}
 	>
 		<FormGroup>
-			<Field name="title" placeholder='Title' tag={Input} />
+			<Field name="title" placeholder="Title" tag={Input} />
 		</FormGroup>
 
 		<FormGroup>
 			<Field name="text" tag={Textarea} />
 		</FormGroup>
 
-		<button>{toggle ? '✓' : '+'}</button>
-		{toggle && <button onClick={preventingDefault(toggle)}>×</button>}
-		{deleteCard &&
-			<button onClick={preventingDefault(() => deleteCard(card))}>
-				🗑
-			</button>}
+		<List>
+			<Button colour={card._id ? 'sky' : 'apple'}>
+				<LabelBody>
+					{card._id ? <Icon icon="ion-checkmark" /> : <Icon icon="ion-plus" />}
+					{card._id ? 'Save' : 'Add'} card
+				</LabelBody>
+			</Button>
+			{toggle &&
+				<Button onClick={preventingDefault(toggle)} colour="steel">
+					<LabelBody>
+						<Icon icon="ion-close" /> Cancel
+					</LabelBody>
+				</Button>}
+			{deleteCard &&
+				<Button
+					onClick={preventingDefault(() => deleteCard(card))}
+					colour="scarlet"
+				>
+					<LabelBody>
+						<Icon icon="ion-trash-a" /> Delete
+					</LabelBody>
+				</Button>}
+		</List>
 	</Form>;
+
+const EditCardContainer = createContainer(
+	() => ({
+		deleteCard(card) {
+			Cards.remove(card._id);
+		},
+	}),
+	EditCard
+);
 
 const ShowCard = ({
 	card,
@@ -54,26 +90,50 @@ const ShowCard = ({
 		{card.relatedTypes &&
 			<List>
 				{card.relatedTypes.map(type =>
-					<Label key={type._id} {...type.colour}>{type.name}</Label>
+					<Label key={type._id} {...type.colour}>
+						{type.name}
+					</Label>
 				)}
 			</List>}
 
-		{toggle && <button onClick={toggle}>Edit</button>}
-		<h1><a href={`#${card._id}`} onClick={() => selectCard(card)}>{card.title}</a></h1>
+		{toggle && <Button onClick={toggle}>
+			<LabelBody>
+				<Icon icon='ion-edit' />
+			</LabelBody>
+		</Button>}
+		<h1>
+			<a href={`#${card._id}`} onClick={() => selectCard(card)}>
+				{card.title}
+			</a>
+		</h1>
 		<Markdown source={card.text || ''} />
 
 		{linkTypes.map(
 			type =>
 				related[type._id] &&
 				<div key={type._id}>
-					<Label {...type.colour}>{type.name}</Label>
+					<Label {...type.colour}>
+						{type.name}
+					</Label>
 
 					<ul>
 						{related[type._id].map(({card, related}) =>
 							<li key={card._id}>
-								<a href={`#${card._id}`} onClick={() => selectCard(card)}>{card.title}</a>
+								<a
+									href={`#${card._id}`}
+									onClick={() => selectCard(card)}
+								>
+									{card.title}
+								</a>
 
-								<a href='#' onClick={preventingDefault(() => removeRelated(related))}>×</a>
+								<a
+									href="#"
+									onClick={preventingDefault(() =>
+										removeRelated(related)
+									)}
+								>
+									×
+								</a>
 							</li>
 						)}
 					</ul>
@@ -86,7 +146,12 @@ const ShowCard = ({
 			<CardSelect skip={[card._id]} />
 
 			{/* TODO: card creation by adding link */}
-			<button>+</button>
+
+			<Button colour='aqua'>
+				<LabelBody>
+					<Icon icon="ion-link" /> Link
+				</LabelBody>
+			</Button>
 		</Form>
 	</div>;
 
@@ -124,7 +189,11 @@ const ShowCardContainer = createContainer(({card}) => {
 
 const Card = props =>
 	<CardPrimitive large={props.large}>
-		<Toggler active={EditCard} inactive={ShowCardContainer} {...props} />
+		<Toggler
+			active={EditCardContainer}
+			inactive={ShowCardContainer}
+			{...props}
+		/>
 	</CardPrimitive>;
 
 export default Card;
