8538045bd8df3c237406cdc676ad259eca156282
Matt Brennan <matt@153.io>
2017-08-29 22:40:31 +0100

    shitty broken meteor

---
+++ pages/home.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import Group from '../src/group';
+
+export default () => <Group which='display' />;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,21 @@
-import React from 'react';
-import Group from '../src/group';
+import styleSheet from 'styled-components/lib/models/StyleSheet';
+import {injectGlobal} from 'styled-components'
+import {background} from '../src/colors';
+import {FlowRouter} from 'meteor/kadira:flow-router';
 
-export default () => <Group which='display' />;
+injectGlobal`
+body {
+	font-family: Bookmania;
+	line-height: 1.6;
+	font-size: 24px;
+	margin: 0;
+	overflow: hidden;
+	background: ${background};
+}
+
+* {
+	box-sizing: border-box;
+}
+`;
+
+console.log(FlowRouter);
