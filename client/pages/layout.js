1cd6fb3d097dbb9da6501fd925da9d071fb95158
Matt Brennan <matt@153.io>
2017-12-19 14:11:22 +0000

    nav

--- client/pages/layout.js
+++ client/pages/layout.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import App from './app';
+import {withCampaign} from '../components/campaign';
 
-const Layout = ({children}) => <App>
-	<nav>toolbar lol</nav>
+const Nav = withCampaign(({campaignId}) => <nav>
+	<a href={`/`}>Campaigns</a>
+	<a href={`/${campaignId}`}>Cards</a>
+	<a href={`/${campaignId}/dashboard`}>Dashboard</a>
+	<a href={`/${campaignId}/dashboard-control`}>Dashboard Control</a>
+</nav>);
+
+const Layout = ({children, campaignId}) => <App campaignId={campaignId}>
+	<Nav />
 	{children}
 </App>;
 
