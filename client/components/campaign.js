942772adabc0cf039f55c8fc5cade3114f1400d2
Matt Brennan <matt@153.io>
2017-12-26 16:57:56 +0000

    refactor campaign.js using recompose

--- client/components/campaign.js
+++ client/components/campaign.js
@@ -1,38 +1,29 @@
 import PropTypes from 'prop-types';
 import React, {Component} from 'react';
-import {compose} from 'recompose';
+import {
+	compose,
+	withProps,
+	withContext,
+	getContext
+} from 'recompose';
 import getCampaignSession from '../../shared/session';
 
 export const campaignContext = {
 	campaignId: PropTypes.string,
 };
 
-export const withCampaign = Child => {
-	const Wrapped = (props, {campaignId}) => <Child {...props} campaignId={campaignId} />;
-	Wrapped.contextTypes = campaignContext;
-	return Wrapped;
-};
+export const withCampaign = getContext(campaignContext);
 
-export const setsCampaign = Child => class Wrapped extends Component {
-	static childContextTypes = {
-		campaignId: PropTypes.string,
-	};
+export const setsCampaign = withContext(
+	campaignContext,
+	({campaignId}) => ({campaignId})
+);
 
-	getChildContext() {
-		return {
-			campaignId: this.props.campaignId,
-		};
-	}
-
-	render() {
-		const {campaignId, ...props} = this.props;
-		return <Child {...props} />;
-	}
-};
+const setCampaignSession = withProps(({campaignId}) => ({
+	campaignSession: getCampaignSession(campaignId)
+}));
 
 export const withCampaignSession = compose(
-	Child => ({campaignId, ...props}) => (
-		<Child {...props} campaignSession={getCampaignSession(campaignId)} />
-	),
+	setCampaignSession,
 	withCampaign
 );
