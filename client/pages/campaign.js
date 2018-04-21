4c52b02c2d924dfe11fc57db9708b79a2ee41c3c
Matt Brennan <matt@153.io>
2018-04-21 14:34:15 +0100

    basic campaign splash with randomish image from some stuff taken from unsplash

--- client/pages/campaign.js
+++ client/pages/campaign.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import {withCampaignData} from '../data/campaign';
+import unsplashImages from '../visual/unsplash.json';
+import {withProps} from 'recompact';
+import styled from 'styled-components';
+import stringHash from 'string-hash';
+
+const Splash = withProps(({campaignId}) => {
+	const image = unsplashImages[stringHash(campaignId) % unsplashImages.length];
+
+	return {
+		img: image.urls.regular,
+		color: image.color,
+	};
+})(styled.div`
+	background: url(${({img}) => img}), ${({color}) => color};
+	background-size: cover;
+	width: 100vw;
+	height: 30vw;
+	max-height: 40vh;
+	background-position: center;
+`);
 
 export default withCampaignData(({campaign}) => <div>
-	{campaign.title}
+	<Splash campaignId={campaign._id} />
 </div>);
