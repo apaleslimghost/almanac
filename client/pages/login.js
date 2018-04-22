135ddf399cf97112ba1cffe3367ea02aed37e815
Matt Brennan <matt@153.io>
2018-04-22 15:49:29 +0100

    beginnings of a custom login form

--- client/pages/login.js
+++ client/pages/login.js
@@ -1,8 +1,30 @@
 import React from 'react';
-import {LoginBox} from 'meteor/universe:accounts-ui';
+import {Meteor} from 'meteor/meteor';
 import {Accounts} from 'meteor/accounts-base'
 import {navigate} from 'meteor/quarterto:reactive-history';
+import {Input as BaseInput, Button} from '../visual/primitives';
+import {Form, Field} from '../control/form';
 
-Accounts.onLogin(() => navigate('/'));
+const doLogin = ({userOrEmail, password}) => {
+	Meteor.loginWithPassword(userOrEmail, password, err => {
+		if(err) {
+			alert(err.reason);
+		} else {
+			navigate('/');
+		}
+	})
+};
 
-export default () => <LoginBox />;
+const Input = props => <Field tag={BaseInput} {...props} />;
+
+export default () => <Form onSubmit={doLogin}>
+	<label>
+		Username or email
+		<Input name='userOrEmail' placeholder='user@example.com' />
+	</label>
+	<label>
+		Password
+		<Input name='password' placeholder='secret' type='password' />
+	</label>
+	<Button>Log in</Button>
+</Form>;
