3f54c8de5fceb297d22b6bab015fb2e6d169b87d
Matt Brennan <matt@153.io>
2018-05-28 09:16:02 +0100

    more grid lines

--- client/visual/grid.js
+++ client/visual/grid.js
@@ -11,10 +11,10 @@ export default styled.div`
 	display: grid;
 	width: 100%;
 
-	grid-template-columns: [bleed-start] 1fr [content-start] minmax(auto, 60em) [content-end] 1fr [bleed-end];
+	grid-template-columns: [bleed-start] 1fr [full-start left-start left-two-start] minmax(auto, 15em) [left-end center-start right-two-start]  minmax(auto, 30em) [center-end left-two-end right-start] minmax(auto, 15em) [full-end right-end right-two-end] 1fr [bleed-end];
 	grid-gap: 1em;
 
 	> :not(.${bleed}) {
-		grid-column: content;
+		grid-column: full;
 	}
 `;
