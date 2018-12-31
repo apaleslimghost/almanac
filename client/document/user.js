a4ef1b7f46f6bf5b012a62a8180c6a4de70abd39
bren <bren@153.io>
2018-12-31 21:55:54 +0000

    but their emails

--- client/document/user.js
+++ client/document/user.js
@@ -4,15 +4,22 @@ import colours from '@quarterto/colours'
 import Gravatar from '../visual/gravatar'
 import { Label, LabelBody } from '../visual/primitives'
 import { withOwnerData } from '../data/owner'
+import { withLoadingComponent } from '../control/loading'
+import { withSubscribe } from '../utils/subscribe'
+import { compose } from 'recompact';
 
 const UserText = styled.span`
 	font-style: ${({ verified }) => (verified ? 'normal' : 'italic')};
 	color: ${({ verified }) => (verified ? colours.steel[0] : colours.steel[2])};
 `
 
+// styled-components correctly handles arbitrary props passed to dom nodes
+// and i can't be fucked working out how to implement that so
+const Slurp = styled.span``
+
 const User = ({
 	user,
-	component: Component = 'span',
+	component: Component = Slurp,
 	children,
 	...props
 }) => <Component user={user} {...props}>
@@ -32,6 +39,10 @@ const User = ({
 
 export default User
 
-export const Owner = withOwnerData(
-	props => props.of
-)(User)
+const connectOwner = compose(
+	withSubscribe('campaigns.members'),
+	withOwnerData('of'),
+	withLoadingComponent(null)
+)
+
+export const Owner = withOwnerData('of')(User)
--- client/utils/subscribe.js
+++ client/utils/subscribe.js
@@ -1,4 +1,10 @@
+import { withTracker } from 'meteor/react-meteor-data'
 import { Meteor } from 'meteor/meteor'
 
-export default (...subs) =>
+const subscribe = (...subs) =>
 	subs.map(name => Meteor.subscribe(name)).every(sub => sub.ready())
+
+export default subscribe
+export const withSubscribe = (...subs) => withTracker({
+	ready: subscribe(...subs)
+})
