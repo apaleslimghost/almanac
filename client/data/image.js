2ff74a2b7675f5c1f1fe49af0daebd3b2f6d6d9a
bren <bren@153.io>
2019-01-01 20:54:37 +0000

    card cover images in actual card cards wow

--- client/data/image.js
+++ client/data/image.js
@@ -2,9 +2,12 @@ import { withTracker } from 'meteor/react-meteor-data'
 import subscribe from '../utils/subscribe'
 import { UnsplashPhotos } from '../../shared/collections';
 
-export default getImageId => withTracker(props => ({
-	ready: subscribe(
-		['unsplash.getPhoto', getImageId(props)]
-	),
-	image: UnsplashPhotos.findOne(getImageId(props)),
-}))
+export default getImageId => withTracker(props => {
+	const imageId = getImageId(props)
+	return imageId ? {
+		ready: subscribe(
+			['unsplash.getPhoto', imageId]
+		),
+		image: UnsplashPhotos.findOne(imageId),
+	} : {}
+})
--- client/document/card.js
+++ client/document/card.js
@@ -1,23 +1,51 @@
 import React from 'react'
 import _ from 'lodash'
+import styled, { css } from 'styled-components'
 
 import Link from '../control/link'
-
-import { Card as CardPrimitive, Label, LabelBody } from '../visual/primitives'
+import withImage from '../data/image'
+import { SplashBackground, Hero, HeroTitle } from '../visual/splash'
+import { Card as CardPrimitive } from '../visual/primitives'
 import Markdown from './markdown'
 
-export default ({ card }) => (
-	<CardPrimitive>
-		<Label colour='sky'>
-			<LabelBody>{card.type}</LabelBody>
-		</Label>
+const CardHeader = withImage(({ card }) => card.cover)(SplashBackground.extend`
+	margin: -1rem -1rem 0;
+	padding-top: 0.5rem;
+	transition: filter 0.2s;
+	will-change: filter;
+	border-top-left-radius: 2px;
+	border-top-right-radius: 2px;
 
-		<article>
-			<h1>
-				<Link href={`/${card.campaignId}/${card._id}`}>{card.title}</Link>
-			</h1>
+	${({ image }) => image && css`
+		height: 6rem;
+	`}
+
+	a:hover & {
+		filter: contrast(120%) brightness(95%) saturate(110%);
+	}
+
+	&:last-child {
+		margin-bottom: -1rem;
+		border-bottom-left-radius: 2px;
+		border-bottom-right-radius: 2px;
+	}
+`)
+
+const PlainLink = styled(Link)`
+	text-decoration: none;
+	color: inherit;
+`
+
+export default ({ card }) => (
+	<PlainLink href={`/${card.campaignId}/${card._id}`}>
+		<CardPrimitive>
+			<CardHeader card={card}>
+				<Hero>
+					<HeroTitle>{card.title}</HeroTitle>
+				</Hero>
+			</CardHeader>
 
 			<Markdown excerpt source={card.text || ''} />
-		</article>
-	</CardPrimitive>
+		</CardPrimitive>
+	</PlainLink>
 )
