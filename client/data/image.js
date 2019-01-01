fd4c073ef14d8a81e36e51fdfa42cf36e538fe5b
bren <bren@153.io>
2019-01-01 22:15:43 +0000

    backwards compatible images

--- client/data/image.js
+++ client/data/image.js
@@ -2,18 +2,28 @@ import { withTracker } from 'meteor/react-meteor-data'
 import subscribe from '../utils/subscribe'
 import { UnsplashPhotos } from '../../shared/collections'
 
+const getImageSubscription = image => {
+	switch (image.from) {
+		case undefined:
+			// backwards compatibility for implicit unsplash images
+			return getImageSubscription({ from: 'unsplash', id: image })
+
+		case 'unsplash':
+			return {
+				ready: subscribe(['unsplash.getPhoto', image.id]),
+				image: UnsplashPhotos.findOne(image.id)
+			}
+	}
+
+	return {}
+}
+
 export default getImageId =>
 	withTracker(props => {
 		const image = getImageId(props)
 
 		if (image) {
-			switch (image.from) {
-				case 'unsplash':
-					return {
-						ready: subscribe(['unsplash.getPhoto', image.id]),
-						image: UnsplashPhotos.findOne(image.id)
-					}
-			}
+			return getImageSubscription(image)
 		}
 
 		return {}
