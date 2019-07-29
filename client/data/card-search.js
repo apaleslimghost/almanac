02b0cf4e9befcee31b84f27dc521c274b6adb745
bren <bren@153.io>
2019-07-29 21:21:28 +0100

    card list

--- client/data/card-search.js
+++ client/data/card-search.js
@@ -1,4 +1,5 @@
 import { withTracker } from 'meteor/react-meteor-data'
+import { useTracker } from 'meteor/quarterto:hooks'
 import subscribe from '../utils/subscribe'
 import { Cards } from '../../shared/collections'
 
@@ -17,3 +18,21 @@ const withCardSearch = withTracker(({ search, campaignId }) => ({
 }))
 
 export default withCardSearch
+
+export const useCardSearch = ({ search, campaignId }) =>
+	useTracker(
+		() => ({
+			ready: subscribe(['cards.all', search]),
+			cards: Cards.find(
+				{ campaignId },
+				search
+					? {
+							sort: [['score', 'desc']],
+					  }
+					: {
+							sort: [['title', 'asc']],
+					  },
+			).fetch(),
+		}),
+		[search, campaignId],
+	)
