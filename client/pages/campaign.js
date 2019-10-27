c327f9069bb63661561262022cae8278423c9165
bren <bren@153.io>
2019-10-27 17:58:45 +0000

    campaign search

--- client/pages/campaign.js
+++ client/pages/campaign.js
@@ -1,9 +1,8 @@
-import React from 'react'
-import { withTracker } from 'meteor/react-meteor-data'
-import { compose, withHandlers } from 'recompact'
+import React, { useEffect, useState } from 'react'
+import { useTracker } from 'meteor/quarterto:hooks'
 import _ from 'lodash'
 
-import { withCampaignData } from '../data/campaign'
+import { useCampaign } from '../data/campaign'
 import { CampaignSplash } from '../visual/splash'
 import Title from '../utils/title'
 import CardList from '../collection/card-list'
@@ -14,44 +13,34 @@ import HistoryList from '../collection/card-history'
 import { Main, Aside } from '../visual/grid'
 import { Card } from '../../shared/methods'
 import { go, state, history } from '../utils/router'
-import { withState } from 'recompact'
-import { withPropsOnChange } from 'recompact'
 
-const withSearchState = withState('_search', '_setSearch', '')
 const setSearch = search => go(history.get(), { search })
 const debouncedSetSearch = _.debounce(setSearch, 300)
 
-const withCampaignSearch = withTracker(({ _setSearch }) => ({
-	search: (state.get() || {}).search,
-	setSearch(search) {
+export default () => {
+	const campaign = useCampaign()
+	const { search } = useTracker(() => state.get() || {})
+	const [_search, _setSearch] = useState('')
+
+	function setSearch(search) {
 		_setSearch(search)
 		debouncedSetSearch(search)
-	},
-}))
+	}
 
-const withCreateCardSearchAction = withHandlers({
-	searchAction: ({ search, setSearch, campaign }) => async () => {
+	useEffect(() => {
+		_setSearch(search)
+	}, [search])
+
+	async function searchAction() {
 		const { _id } = await Card.create({
 			title: search,
 			campaignId: campaign._id,
 		})
 		setSearch('')
 		go(`/${campaign._id}/${_id}`)
-	},
-})
+	}
 
-const connectCampaignPage = compose(
-	withCampaignData,
-	withSearchState,
-	withCampaignSearch,
-	withCreateCardSearchAction,
-	withPropsOnChange('search', ({ search, _setSearch }) => {
-		_setSearch(search)
-	}),
-)
-
-export default connectCampaignPage(
-	({ campaign, search, _search, setSearch, searchAction }) => (
+	return (
 		<>
 			<Title>{campaign.title}</Title>
 
@@ -80,5 +69,5 @@ export default connectCampaignPage(
 				<HistoryList />
 			</Aside>
 		</>
-	),
-)
+	)
+}
