47a6804e7f0abfd9daa283d8a6356bca12b0628d
bren brightwell <bren@153.io>
2019-11-04 17:33:21 +0000

    Create notes.js

---
+++ client/blocks/notes.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Textarea = styled.textarea`
+width: 100%;
+height: 100%;
+`
+
+export control = () => <Textarea />
+export display = () => null
