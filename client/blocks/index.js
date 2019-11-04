630e9cc50daa3e9e8e256ab2782fd7a58513a0ad
bren brightwell <bren@153.io>
2019-11-04 17:34:15 +0000

    Update index.js

--- client/blocks/index.js
+++ client/blocks/index.js
@@ -3,5 +3,6 @@ import * as quests from './quests'
 import * as weather from './weather'
 import * as factions from './factions'
 import * as clock from './clock'
+import * as notes from './notes'
 
-export { time, quests, weather, factions, clock }
+export { time, quests, weather, factions, clock, notes }
