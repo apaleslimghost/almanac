65a166390d061580b9280cf1d6e544a649ec84b8
bren <bren@153.io>
2019-11-08 08:23:35 +0000

    new campaign

--- client/pages/new-campaign.js
+++ client/pages/new-campaign.js
@@ -1,14 +1,15 @@
-import { withHandlers } from 'recompact'
+import React from 'react'
 import { navigate as go } from 'use-history'
 import CampaignSettings from '../document/campaign-settings'
 import { Campaign } from '../../shared/methods'
 
-const withCampaignActions = withHandlers({
-	onSubmit: () => async data => {
-		data.member = []
-		const { _id } = await Campaign.create(data)
-		go(`/${_id}`)
-	},
-})
-
-export default withCampaignActions(CampaignSettings)
+export default props => (
+	<CampaignSettings
+		onSubmit={async data => {
+			data.member = []
+			const { _id } = await Campaign.create(data)
+			go(`/${_id}`)
+		}}
+		{...props}
+	/>
+)
