52388844a57bdddabf50a4b8e0ca154615187b5d
bren <bren@153.io>
2019-12-16 21:50:52 +0000

    storybook global styles

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,14 @@
+import React from 'react'
 import { configure } from '@storybook/react'
+import { addDecorator } from '@storybook/react'
+import GlobalStyles from '../client/visual/global'
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../stories', true, /\.stories\.js$/), module)
+
+addDecorator(storyFn => (
+	<>
+		<GlobalStyles />
+		{storyFn()}
+	</>
+))
