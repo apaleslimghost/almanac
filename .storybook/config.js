d5836b13e81ab09c2a664016c4d190d7128e1e23
bren <bren@153.io>
2020-05-26 22:47:29 +0100

    menu story

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,17 +1,13 @@
 import React from 'react'
-import styled from 'styled-components'
 import { configure, addDecorator } from '@storybook/react'
 import GlobalStyles from '../imports/ui/visual/global'
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../stories', true, /\.stories\.js$/), module)
-const Wrapper = styled.div`
-	margin: 1rem;
-`
 
 addDecorator(storyFn => (
-	<Wrapper>
+	<>
 		<GlobalStyles />
 		{storyFn()}
-	</Wrapper>
+	</>
 ))
