a84a24dc19408b4d6adb5180939f5e5fdaffff3e
Kara Brightwell <kara@153.io>
2021-01-02 22:41:45 +0000

    add stimulus; image lazy loading

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,9 @@
 import turbolinks from 'turbolinks'
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
 
 turbolinks.start()
+
+const application = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
---
+++ app/javascript/packs/controllers/lazy-image-controller.js
@@ -0,0 +1,52 @@
+import { Controller } from 'stimulus'
+import { decode } from 'blurhash'
+
+const observer = new IntersectionObserver(entries => {
+	for (const entry of entries) {
+		if (entry.isIntersecting) {
+			entry.target['lazy-image'].loadImage()
+		}
+	}
+})
+
+export default class LazyImage extends Controller {
+	static values = { blurhash: String, src: String }
+	static targets = ['canvas', 'image']
+
+	connect() {
+		this.element[this.identifier] = this
+
+		if (!this.imageTarget.src) {
+			this.ctx = this.canvasTarget.getContext("2d")
+			this.renderBlur()
+			observer.observe(this.element)
+		}
+	}
+
+	renderBlur() {
+		const pixels = decode(this.blurhashValue, 32, 32);
+		const imageData = this.ctx.createImageData(32, 32);
+		imageData.data.set(pixels);
+		this.ctx.putImageData(imageData, 0, 0);
+	}
+
+	loadImage() {
+		this.imageTarget.src = this.srcValue
+		if (this.imageTarget.complete) {
+			this.showImageImmediately()
+		}
+	}
+
+	showImage() {
+		this.element.classList.add('loaded')
+	}
+
+	showImageImmediately() {
+		this.showImage()
+		this.element.classList.add('loaded-instant')
+	}
+
+	disconnect() {
+		observer.unobserve(this.element)
+	}
+}
