9cd85d7b6e2b9f7fd3dfeb1eb179fb1e72a94b2f
Kara Brightwell <kara@153.io>
2021-01-10 20:31:03 +0000

    implement link type select correctly

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,16 +1,19 @@
 class LinksController < ApplicationController
   def create
     @link = Link.find_or_initialize_by(link_params)
-
     @link.save
     redirect_to [@link.from.campaign, @link.from]
   end
 
-  def destroy; end
+  def destroy
+    @link = Link.find(params[:id])
+    @link.destroy
+    redirect_to [@link.from.campaign, @link.from]
+  end
 
   private
 
   def link_params
-    params.require(:link).permit(:from_id, :to_id, :link)
+    params.require(:link).permit(:from_id, :to_id, :link_type)
   end
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,22 @@
 class Link < ApplicationRecord
   belongs_to :from
   belongs_to :to
+
+  def link_type=(new_type)
+    return unless Link.types.include? new_type
+
+    Link.types.each do |type|
+      self[type] = type == new_type ? 1 : 0
+    end
+  end
+
+  def link_type
+    Link.types.find do |type|
+      self[type] == 1
+    end
+  end
+
+  def self.types
+    _dag_options.types.keys
+  end
 end
