2dfe808a143ff49df46ccee8cc23c12555822bb8
Kara Brightwell <kara@153.io>
2021-05-31 08:54:24 +0100

    only show this campaign's cards in selects

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -5,4 +5,8 @@ class Campaign < ApplicationRecord
   has_many :cards
   has_one :image, as: :imageable
   accepts_nested_attributes_for :image, reject_if: proc { |attributes| !Image.valid_params? attributes }
+
+  has_many :quests, source_type: 'CardType::Quest', through: :cards, source: :actable
+  has_many :locations, source_type: 'CardType::Location', through: :cards, source: :actable
+  has_many :objectives, source_type: 'CardType::Objective', through: :cards, source: :actable
 end
