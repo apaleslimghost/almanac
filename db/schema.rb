0dfb7e78efc46c1d6bf02de1585071bf2326697b
Kara Brightwell <kara@153.io>
2021-06-05 14:18:11 +0100

    add index on usernames

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_05_125656) do
+ActiveRecord::Schema.define(version: 2021_06_05_131611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -107,6 +107,7 @@ ActiveRecord::Schema.define(version: 2021_06_05_125656) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "campaign_settings", "campaigns"
