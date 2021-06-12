385ddea4abdc244e6734e95fd21e56e1dc7ef86c
Kara Brightwell <kara@153.io>
2021-06-12 11:35:32 +0100

    fix creating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -61,6 +61,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation)
+      params.require(:user).permit(:email, :username, :password, :password_confirmation)
     end
 end
