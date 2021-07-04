253e44cc50e01f4c390feda9d3b9b45c28f7f132
Kara Brightwell <kara@153.io>
2021-07-04 08:34:50 +0100

    don't show the splash image for user pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,6 +51,7 @@ class UsersController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find_by_username(params[:id])
+      @image = nil
     end
 
     # Only allow a list of trusted parameters through.
