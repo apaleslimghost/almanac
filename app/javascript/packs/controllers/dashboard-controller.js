ed1687518f6221b182b0c71a41e71914425e4837
Kara Brightwell <kara@153.io>
2021-07-07 12:07:55 +0100

    just reload the entire dashboard lol

--- app/javascript/packs/controllers/dashboard-controller.js
+++ app/javascript/packs/controllers/dashboard-controller.js
@@ -1,19 +1,22 @@
 import { Controller } from 'stimulus'
+import Turbolinks from 'turbolinks'
 import consumer from '../consumer'
 
 export default class Dashboard extends Controller {
 	static values = { slug: String }
 
-   connect() {
-      this.subscription = consumer.subscriptions.create({
-         channel: "DashboardChannel",
+	connect() {
+		this.subscription = consumer.subscriptions.create({
+			channel: "DashboardChannel",
 			slug: this.slugValue
-      }, {
-         received: console.log
-      })
-   }
+		}, {
+			received: () => {
+				Turbolinks.visit(location.pathname, { action: 'replace' })
+			}
+		})
+	}
 
-   disconnect() {
-      this.subscription.unsubscribe()
-   }
+	disconnect() {
+		this.subscription.unsubscribe()
+	}
 }
