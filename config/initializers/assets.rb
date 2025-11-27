0cbf88c8ccd8f26c77f097f09956b6c91da7802d
Kara Brightwell <kara@153.io>
2025-11-27 22:17:48 +0000

    fix rpg-awesome in prod

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
 Rails.application.config.assets.paths << Rails.root.join("node_modules/@fortawesome/fontawesome-free/webfonts")
+Rails.application.config.assets.paths << Rails.root.join("node_modules/rpg-awesome/fonts")
